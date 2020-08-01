8fc988fef760915e9d08559ce901761d52ec65c6
xTRiM <xTRiM@users.noreply.github.com>
2020-08-02 00:49:41 +0300

    Removed Timeout & Events, added direct Entity ops

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 'use strict';
 
-const EventEmitter = require('events');
-
 class Entity {
 	constructor(data, duration){
 		this.data = data
@@ -14,42 +12,31 @@ class Entity {
 }
 
 class Cache extends Map {
-	constructor(values, capacity = 100){
+	constructor(values){
 		super(values)
-		this.events = new EventEmitter()
-		this.capacity = capacity
 	}
+	
 	set(key, value, duration){
 		var entity = new Entity(value, duration)
 		super.set(key, entity)
-		this.events.emit('save', key, value, duration)
-		if(this.size > this.capacity) this.clean()
-		if(duration) setTimeout(key => {
-			const o = super.get(key)
-			if(o && !o.expired) console.log(`${key} is not expired!`, Date.now() - o.expire)
-			if(o && o.expired) this.delete(key); 
-		}, duration, key)
 	}
 
 	get(key){
+		var entity = this.get_entity(key);
+		return entity === undefined || entity.data;
+	}
+	
+	set_entity(key, entity){
+		super.set(key, entity)
+	}
+	
+	get_entity(key){
 		var entity = super.get(key);
-		return entity === undefined || entity.expired ? undefined : entity.data;
+		return entity === undefined || entity.expired ? undefined : entity;
 	}
 
 	delete(key){
-		this.events.emit('delete', key, super.get(key).data)
 		super.delete(key)
-	}
-	clean(){
-		var keys = this.keys();
-		while(this.size > this.capacity){
-			var key = keys.next().value;
-			this.delete(key)
-		}
-	}
-
-	on(event, callback){
-		this.events.on(event, callback);
 	}
 }
 
